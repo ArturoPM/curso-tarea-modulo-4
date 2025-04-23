2f5409af8639b0e054df83f6366027b5c274e69c
Arturo Peña <artzcoder@gmail.com>
2025-04-23 14:02:43 -0600

    23-04-2025 -> Tarea Modulo 4

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -12,37 +12,62 @@
             <div class="col-md-12">
                 <div class="mb-3">
                     <label for="name" class="form-label">Nombre del usuario</label>
-                    <input type="text" class="form-control" id="name" name="name" placeholder="Ingresa el nombre del usuario" value="{{old('name', $usuario->name)}}">
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Ingresa el nombre del usuario" value="{{old('name', $usuario->name)}}">
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <div class="col-md-12">
                 <div class="mb-3">
                     <label for="email" class="form-label">Correo electrónico del usuario</label>
-                    <input type="text" class="form-control" id="email" name="email" placeholder="Ingresa el correo electrónico del usuario" value="{{old('email', $usuario->email)}}">
+                    <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Ingresa el correo electrónico del usuario" value="{{old('email', $usuario->email)}}">
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="mb-3">
                     <label for="age" class="form-label">Edad del usuario</label>
-                    <input type="number" class="form-control" id="age" name="age" placeholder="Ingresa la edad del usuario" value="{{old('age', $usuario->age)}}">
+                    <input type="number" class="form-control @error('age') is-invalid @enderror" id="age" name="age" placeholder="Ingresa la edad del usuario" value="{{old('age', $usuario->age)}}">
+                    @error('age')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="mb-3">
                     <label for="position" class="form-label">Cargo laboral</label>
-                    <input type="text" class="form-control" id="position" name="position" placeholder="Ingresa el cargo laboral del usuario" value="{{old('position', $usuario->position)}}">
+                    <input type="text" class="form-control @error('position') is-invalid @enderror" id="position" name="position" placeholder="Ingresa el cargo laboral del usuario" value="{{old('position', $usuario->position)}}">
+                    @error('position')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="mb-3">
                     <label for="password" class="form-label">Contraseña (Opcional)</label>
-                    <input type="text" class="form-control" id="password" name="password" placeholder="Ingresa la contraseña para el usuario">
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Ingresa la contraseña para el usuario">
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="mb-3">
                     <label for="password_confirmation" class="form-label">Confirmar contraseña</label>
-                    <input type="text" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Ingresa la contraseña para el usuario">
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password_confirmation" name="password_confirmation" placeholder="Ingresa la contraseña para el usuario">
                 </div>
             </div>
             <div class="col-md-6">
